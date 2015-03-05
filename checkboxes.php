64e6157d769a79f00341f46a7db254e1095b5513
James Van Waza <jgacuca@gmail.com>
2015-03-05 16:42:59 -0500

    Added div row class

--- checkboxes.php
+++ checkboxes.php
@@ -1,11 +1,15 @@
 <?php include 'html5req.php';?>
+<h1>Which Hobbies fo you like</h1>
 <form action="" id="webForm" method="POST">
-	Which you do you want to likes?<br />
-	<input type="checkbox" name="Hobbies[]" value="Books" />Books<br />
-	<input type="checkbox" name="Hobbies[]" value="Movies" />Movies<br />
-	<input type="checkbox" name="Hobbies[]" value="Sports" />Sports<br />
-	<input type="checkbox" name="Hobbies[]" value="Games" />Games<br />
-	<input type="checkbox" name="Hobbies[]" value="Travelling" />Travelling<br />
-	<br />
-	<button class="submit-btn">Submit</button>
+	<div class="row">
+		<div class="large-12 columns">
+			<input type="checkbox" name="Hobbies[]" value="Books" />Books<br />
+			<input type="checkbox" name="Hobbies[]" value="Movies" />Movies<br />
+			<input type="checkbox" name="Hobbies[]" value="Sports" />Sports<br />
+			<input type="checkbox" name="Hobbies[]" value="Games" />Games<br />
+			<input type="checkbox" name="Hobbies[]" value="Travelling" />Travelling<br />
+			<br />
+			<button class="submit-btn">Submit</button>
+		</div>
+	</div>
 </form>
