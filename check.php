570c2383806a96be311b3de702edd20cb6a0c76d
James Van Waza <jgacuca@gmail.com>
2015-03-05 17:55:18 -0500

    Refractored from the register-view users page because mysqli is being used

--- check.php
+++ check.php
@@ -1,32 +1,43 @@
 <?php
+require 'mysqli-connect.php';
 include 'html5req.php';
-if (isset($_POST['submit'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$errors = array(); // Initialize an error array.
+	/*Check whether a hobby has been submitted*/
 	$hobb = $_POST['Hobbies'];
 	if (empty($hobb)) {
-		echo ("<p>You didn't select any any hobby.</p>\n");
+		$errors[] = '<p>' . 'You forgot to enter your hobbies' . '</p>';
 	} else {
-		$N = count($hobb);
-		echo ("<p>You selected $N Hobbies(s): ");
-		for ($i = 0; $i < $N; $i++) {
-			$var1 = $hobb[$i];
-			include 'mysqli-connect.php';
-			$table = "INSERT INTO `nvs`.`checkboxes` (`id`, `hobbie`, `date_created`) VALUES (NULL, '$var1', NOW())";
-			mysqli_query($table) or die(mysqli_error());
-			$inserted_fid = mysqli_insert_id();
-			mysqli_close();
-		}
-		echo "<h3>" . "The Hobbies have been added" . "</h3>";
+		$hobb = $_POST['Hobbies'];
 	}
-} else {
-	echo "<div data-alert class='alert-box alert round'>
-					<h4 class='text-center'>The Hobbies have not been added</h4>
-						<a href='#' class='close'>&times;</a>
-				</div>";
 }
+/*If everything is ok*/
+	if (empty($errors)) {
+		$q = "INSERT INTO checkboxes (id, hobbie, date_created) VALUES ('id', '$var1', NOW())";
+		$result = @mysqli_query($dbcon, $q); // Run the query.
+		if ($result) {
+			$N = count($hobb);
+			echo ("<p>You selected $N Hobbies(s): ");
+			for ($i = 0; $i < $N; $i++) {
+			$var1 = $hobb[$i];
+			}
+		}
+	}
+		echo "<div data-alert class='alert-box success radius'>
+				<h3 class='text-center'>The Hobbies have been added</h3>
+					<a href='#' class='close'>&times;</a>
+			</div>";
+else {
+	echo "<div data-alert class='alert-box alert round'>
+				<h4 class='text-center'>The Hobbies have not been added</h4>
+					<a href='#' class='close'>&times;</a>
+			</div>";
+}
+mysqli_close($dbcon);
 ?>
 <script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/modernizr.js"></script>
-    <script src="js/foundation.min.js"></script>
-    <script>
-        $(document).foundation();
-    </script>
+<script src="js/vendor/modernizr.js"></script>
+<script src="js/foundation.min.js"></script>
+<script>
+    $(document).foundation();
+</script>
