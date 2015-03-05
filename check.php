61a491c34f3f53abdcc8763686b81927d6e89be4
James Van Waza <jgacuca@gmail.com>
2015-03-05 17:13:03 -0500

    Added the script to make sure the jquery works

--- check.php
+++ check.php
@@ -24,3 +24,9 @@ if (isset($_POST['submit'])) {
 				</div>";
 }
 ?>
+<script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/modernizr.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
